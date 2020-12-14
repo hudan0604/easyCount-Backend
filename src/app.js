becb01ae5a9c29040c7e8c2f9caf5b8df5ddb699
Nicolas Hudavert <hudanico@hotmail.com>
2020-12-14 10:04:48 +0100

    feat: separate routes with router

--- src/app.js
+++ src/app.js
@@ -1,55 +1,17 @@
 const express = require('express')
 require('./mongoDB/mongoose')
-const Dashboard = require('./mongoDB/models/dashboard.js');
+
+// routes
+const userRoutes = require('./routes/user.routes');
+const dashboardRoutes = require('./routes/dashboard.routes')
 
 const cors = require('cors');
 
 const app = express()
 app.use(express.json());
+app.use(userRoutes);
+app.use(dashboardRoutes);
 
 app.options('*', cors());
-
-// endpoints
-
-// Add a new dashboard to the dashboards
-app.post('/create-dashboard', (req, res) => {
-    const dashboard = new Dashboard(req.body);
-    dashboard.save()
-        .then(() => {
-            res.send(dashboard)
-        })
-        .catch((error) => {
-                res.send(error)
-        })
-})
-
-// get the list of dashboards
-app.get('/dashboards', (req, res) => {
-    Dashboard.find({}).then((dashboards) => {
-        res.send(dashboards)
-    }).catch((error) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/dashboard/:id', (req, res) => {
-    const _id = req.params.id
-    Dashboard.findById(_id).then((dashboard) => {
-        if (!dashboard) {
-            return res.status(404).send()
-        }
-        res.send(dashboard)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-// delete a dashboard
-app.post('/delete-dashboards', (req, res) => {
-    Dashboard.deleteMany({ _id: { $in: req.body.dashboards } },
-        (error, result) => {
-            error ? res.send(error) : res.send(result)                
-        })
-})
 
 app.listen('3000', () => console.log('app sucessfully listens brooo'));
